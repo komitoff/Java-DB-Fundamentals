78cef2b521c90a973030fd44561f0a2d4f0787fa
komitoff <tobo_91@abv.bg>
2017-05-27 14:18:28 +0300

    Finished minions task

--- Database Basics/Data Types and Queries/minions.sql
+++ Database Basics/Data Types and Queries/minions.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE minions;
+
 CREATE TABLE minions (
     id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(250),
@@ -13,11 +15,16 @@ ALTER TABLE minions
 ADD town_id INT,
 ADD FOREIGN KEY (town_id) REFERENCES towns(id);
 
-INSERT INTO towns(name) VALUES ('Sofia');
-INSERT INTO towns(name) VALUES ('Plovidv');
-INSERT INTO towns(name) VALUES ('Varna');
-
+INSERT INTO towns(id, name) VALUES (1, 'Sofia');
+INSERT INTO towns(id, name) VALUES (2, 'Plovidv');
+INSERT INTO towns(id, name) VALUES (3, 'Varna');
 
 INSERT INTO minions(name, age, town_id) VALUES ('Kevin', 22, 1);
 INSERT INTO minions(name, age, town_id) VALUES ('Bob', 15, 3);
 INSERT INTO minions(name, town_id) VALUES ('Steward', 2);
+
+DELETE FROM minions;
+DELETE FROM towns;
+
+DROP TABLE minions;
+DROP TABLE towns;
