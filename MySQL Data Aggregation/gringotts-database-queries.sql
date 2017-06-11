b35b1538392ae79670993ef860a6d6b8c5dfd260
komitoff <tobo_91@abv.bg>
2017-06-11 20:10:39 +0300

    Did some more tasks

--- MySQL Data Aggregation/gringotts-database-queries.sql
+++ MySQL Data Aggregation/gringotts-database-queries.sql
@@ -61,6 +61,22 @@ ORDER BY
 	wd.magic_wand_creator,
 	wd.deposit_group;
 	
+#9
+USE gringotts;
+SELECT
+	CASE
+		WHEN age BETWEEN 0 AND 10 THEN '[0-10]'
+		WHEN age BETWEEN 11 AND 20 THEN '[11-20]'
+		WHEN age BETWEEN 21 AND 30 THEN '[21-30]'
+		WHEN age BETWEEN 31 AND 40 THEN '[31-40]'
+		WHEN age BETWEEN 41 AND 50 THEN '[41-50]'
+		WHEN age BETWEEN 51 AND 60 THEN '[51-60]'
+		WHEN age BETWEEN 61 AND 10000 THEN '[61+]'
+	END AS age_group, COUNT(wd.first_name) AS wizzard_count
+FROM wizzard_deposits AS wd
+GROUP BY age_group
+ORDER BY age_group;
+	
 #10
 SELECT SUBSTRING(wd.first_name, 1, 1) AS first_letter
 FROM wizzard_deposits AS wd
