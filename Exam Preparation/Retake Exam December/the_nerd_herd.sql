992217bb630aec21354aaaf1caf44b03e78b9018
komitoff <tobo_91@abv.bg>
2017-06-11 13:47:11 +0300

    Updated the nerd_herd database still (18/25)

--- Exam Preparation/Retake Exam December/the_nerd_herd.sql
+++ Exam Preparation/Retake Exam December/the_nerd_herd.sql
@@ -1,13 +1,13 @@
 CREATE DATABASE the_nerd_herd;
 
 CREATE TABLE locations(
-	id INT(13) PRIMARY KEY AUTO_INCREMENT,
-	latitude FLOAT,
-	longitude FLOAT
+	id INT(13) PRIMARY KEY,
+	latitude FLOAT NOT NULL,
+	longitude FLOAT NOT NULL
 );
 
 CREATE TABLE credentials(
-	id INT(13) PRIMARY KEY AUTO_INCREMENT,
+	id INT(13) PRIMARY KEY,
 	email VARCHAR(30) NOT NULL,
 	password VARCHAR(20) NOT NULL
 );
@@ -15,10 +15,10 @@ CREATE TABLE credentials(
 CREATE TABLE users(
 	id INT(13) PRIMARY KEY AUTO_INCREMENT,
 	nickname VARCHAR(25) NOT NULL,
-	gender CHAR(1),
-	age INT(13),
+	gender CHAR(1) NOT NULL CHECK(gender='M' OR gender='F'),
+	age INT(13) NOT NULL,
 	location_id INT(13),
-	credential_id INT(13) UNIQUE,
+	credential_id INT(13) UNIQUE NOT NULL,
 	CONSTRAINT fk_user_location
 	FOREIGN KEY (location_id) REFERENCES locations(id),
 	CONSTRAINT fk_user_credential
@@ -26,16 +26,16 @@ CREATE TABLE users(
 );
 
 CREATE TABLE chats(
-	id INT(13) PRIMARY KEY AUTO_INCREMENT,
-	title VARCHAR(32),
-	start_date DATE,
-	is_active BIT
+	id INT(13) PRIMARY KEY,
+	title VARCHAR(32) NOT NULL,
+	start_date DATE NOT NULL,
+	is_active BIT NOT NULL
 );
 
 CREATE TABLE messages(
-	id INT(13) PRIMARY KEY AUTO_INCREMENT,
+	id INT(13) PRIMARY KEY,
 	content VARCHAR(200) NOT NULL,
-	sent_on DATE, 
+	sent_on DATE NOT NULL , 
 	chat_id INT(13) NOT NULL,
 	user_id INT(13) NOT NULL,
 	CONSTRAINT fk_message_chat
@@ -54,5 +54,7 @@ CREATE TABLE users_chats(
 	CONSTRAINT pk_user_chats 
 	PRIMARY KEY(user_id, chat_id)
 );
+
+
 
 DROP DATABASE the_nerd_herd;
