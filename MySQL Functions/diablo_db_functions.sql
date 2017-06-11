b54e29f9643b6af0867c955c4fae0a6105c38077
komitoff <tobo_91@abv.bg>
2017-06-11 14:50:12 +0300

    Solved some more using regex expressions in mysql.

--- MySQL Functions/diablo_db_functions.sql
+++ MySQL Functions/diablo_db_functions.sql
@@ -5,3 +5,12 @@ FROM games
 WHERE YEAR(start)=2011 OR YEAR(start)=2012
 ORDER BY start
 LIMIT 50;
+
+#13
+
+#14
+USE diablo;
+SELECT u.user_name, u.ip_address
+FROM users AS u
+WHERE u.ip_address REGEXP ('(^[0-9]{3}\\.1[0-9].*\\.[0-9].*\\.[0-9]{3}$)')
+ORDER BY u.user_name;
--- MySQL Functions/geography_functions.sql
+++ MySQL Functions/geography_functions.sql
@@ -1,5 +1,12 @@
-#10
-SELECT `c`.`country_name`, `c`.`iso_code`
-FROM `countries` AS `c`
-WHERE `c`.`country_name` REGEXP '(\\w*([Aa])\\w*\\w*([Aa])\\w*\\w*([Aa])\\w*)'
-ORDER BY `c`.`iso_code`;
+#10 NOT FINISHED
+SELECT c.country_name, c.iso_code
+FROM countries AS c
+WHERE c.country_name NOT REGEXP('[Aa]{0,3}')
+ORDER BY c.iso_code;
+
+#11
+USE geography;
+SELECT p.peak_name, r.river_name, LOWER(CONCAT(p.peak_name, SUBSTRING(r.river_name, 2, CHAR_LENGTH(r.river_name)))) AS mix
+FROM peaks AS p, rivers AS r
+WHERE right(p.peak_name, 1) = LEFT(r.river_name, 1)
+ORDER BY mix;
