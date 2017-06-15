8a6bbbd3000564bc82f09f85c7b0d5b4ca011721
komitoff <tobo_91@abv.bg>
2017-06-15 18:17:55 +0300

    Fixed some bugs on tasks from 1-5 And did few more tasks

--- Subqueries and joins/Homework/qrs_soft_uni_db.sql
+++ Subqueries and joins/Homework/qrs_soft_uni_db.sql
@@ -16,7 +16,7 @@ ON e.department_id = d.department_id
 WHERE d.name LIKE 'Sales'
 ORDER BY e.employee_id DESC;
 
-#3
+#4
 SELECT e.employee_id, e.first_name, e.salary, d.name
 FROM employees AS e
 INNER JOIN departments AS d
@@ -25,8 +25,36 @@ WHERE e.salary > 15000
 ORDER BY e.department_id DESC
 LIMIT 5;
 
-#4
-SELECT *
+#5 NOT FINISHED
+SELECT e.employee_id, e.first_name
 FROM employees AS e
-LEFT OUTER JOIN employees_projects AS ep
-ON ep.employee_id = e.employee_id;
+INNER JOIN employees_projects AS ep
+ON ep.employee_id = e.employee_id
+INNER JOIN projects AS p
+ON ep.project_id = p.project_id
+WHERE ;
+
+#6
+SELECT e.first_name, e.last_name, e.hire_date, d.name AS dept_name
+FROM employees AS e
+INNER JOIN departments AS d
+ON d.department_id = e.department_id
+WHERE DATE(e.hire_date) > '1999/1/1'
+  AND d.name IN ('Finance', 'Sales')
+ORDER BY e.hire_date;
+
+#7
+SELECT e.employee_id, e.first_name, p.name AS project_name 
+FROM employees AS e
+INNER JOIN employees_projects AS ep
+ON ep.employee_id = e.employee_id
+INNER JOIN projects AS p
+ON ep.project_id = p.project_id
+WHERE DATE(p.start_date) > '2002-08-13' 
+  AND p.end_date IS NULL 
+ORDER BY 
+		e.first_name,
+		p.name
+LIMIT 5;
+
+#8
