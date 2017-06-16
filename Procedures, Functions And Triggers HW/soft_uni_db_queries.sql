27c1d9aeca399498e8b8910b8c3a5fc37e9c6d02
komitoff <tobo_91@abv.bg>
2017-06-16 15:05:04 +0300

    Finished some left tasks And started the functions and procedures homework.

---
+++ Procedures, Functions And Triggers HW/soft_uni_db_queries.sql
@@ -0,0 +1,100 @@
+#1
+DELIMITER $$ 
+CREATE PROCEDURE usp_get_employees_salary_above_35000()
+BEGIN 
+	SELECT e.first_name, e.last_name
+	FROM employees AS e
+	WHERE e.salary > 35000
+	ORDER BY
+		e.first_name,
+		e.last_name;
+END $$
+
+DELIMITER ;
+
+CALL usp_get_employees_salary_above_35000();
+
+
+#2 Strange Result
+DELIMITER $$
+CREATE PROCEDURE usp_get_employees_salary_above(number DECIMAL)
+BEGIN
+	SELECT e.first_name, e.last_name
+	FROM employees AS e
+	WHERE e.salary > number
+		OR e.salary = number
+	ORDER BY 
+		e.first_name,
+		e.last_name;
+END $$
+
+DELIMITER ;
+
+CALL usp_get_employees_salary_above(2000.);
+
+#3
+DELIMITER $$
+CREATE PROCEDURE usp_get_towns_starting_with(str VARCHAR(255))
+BEGIN
+	SELECT t.name AS town_name
+	FROM towns AS t
+	WHERE t.name LIKE CONCAT(str, '%')
+	ORDER BY t.name;
+END $$
+DELIMITER ;
+
+CALL usp_get_towns_starting_with('b');
+
+#4
+DELIMITER $$
+CREATE PROCEDURE usp_get_employees_from_town(town_name VARCHAR(255))
+BEGIN
+	SELECT e.first_name , e.last_name
+	FROM employees AS e
+	INNER JOIN addresses AS a
+	ON a.address_id = e.address_id
+	INNER JOIN towns AS t
+	ON a.town_id = t.town_id
+	WHERE t.name LIKE town_name
+	ORDER BY e.first_name;
+END $$
+DELIMITER ;
+
+CALL usp_get_employees_from_town('Sofia');
+
+	SELECT e.first_name , e.last_name
+	FROM employees AS e
+	INNER JOIN addresses AS a
+	ON a.address_id = e.address_id
+	INNER JOIN towns AS t
+	ON a.town_id = t.town_id
+	WHERE t.name LIKE 'Sofia'
+	ORDER BY e.first_name;
+
+#5
+DELIMITER $$
+CREATE FUNCTION ufn_get_salary_level(salary DECIMAL(19,4))
+RETURNS VARCHAR(10)
+BEGIN
+	DECLARE salary_level VARCHAR(10);
+	IF(salary < 30000.0) THEN
+		SET salary_level := 'Low';
+	ELSEIF(salary >= 30000 AND salary <= 50000) THEN
+		SET salary_level := 'Average';
+	ELSE
+		SET salary_level := 'High';
+	END IF;
+	RETURN salary_level;
+END; $$
+DELIMITER ;
+
+SELECT e.employee_id, e.first_name, ufn_get_salary_level(e.salary)
+FROM employees AS e;
+
+
+
+
+
+
+
+
--- Subqueries and joins/Homework/qrs_soft_uni_db.sql
+++ Subqueries and joins/Homework/qrs_soft_uni_db.sql
@@ -66,5 +66,25 @@ INNER JOIN projects AS p
 ON ep.project_id = p.project_id
 
 #9
+UPDATE projects AS pr 
+	SET pr.start_date = IF(pr.start_date > '2005-31-12', null, pr.name);
+	
+	
+SELECT e.employee_id, e.first_name, p.name AS project_name
+FROM employees AS e
+INNER JOIN employees_projects AS ep
+ON e.employee_id = ep.employee_id
+INNER JOIN projects AS p
+ON ep.project_id = p.project_id
+WHERE e.employee_id = 24;
+  
+  
+#9
+SELECT e.employee_id, e.first_name, 
+FROM employees AS e
+INNER JOIN employees AS m
+ON m.employee_id = e.manager_id;
+
+
 
 
