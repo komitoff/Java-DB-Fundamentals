b1907435b327e0a613d547d8cab349d038e2b37a
komitoff <tobo_91@abv.bg>
2017-06-19 16:19:54 +0300

    Did first few tasks from the querying part.

---
+++ Exam Preparation/Exam Prep Part I/Querying_tasks.sql
@@ -0,0 +1,86 @@
+#SECTION 3 - QUERYING
+#5 TICKETS
+SELECT t.ticket_id, t.price, t.class, t.seat
+FROM tickets AS t
+ORDER BY t.ticket_id;
+
+#6 Customers
+SELECT 
+	c.customer_id, 
+	CONCAT_WS(' ', c.first_name, c.last_name) AS 'full_name',
+	c.gender
+FROM customers AS c
+ORDER BY 
+	full_name,
+	c.customer_id;
+	
+#7 FLIGHTS
+SELECT
+	f.flight_id, f.departure_time, f.arrival_time
+FROM 
+	flights AS f
+WHERE 
+	f.`status` LIKE 'Delayed'
+ORDER BY 
+	f.flight_id;
+	
+#8 Airlines
+SELECT DISTINCT
+	a.airline_id, a.airline_name, a.nationality, a.rating
+FROM 
+	airlines AS a
+INNER JOIN flights AS f
+ON f.airline_id=a.airline_id
+ORDER BY 
+	a.rating DESC,
+	a.airline_id
+LIMIT 5;	
+
+#9 First class tickets ----------- NOT FINISHED Incorrect result
+SELECT ti.ticket_id, a.airport_name, CONCAT_WS(' ', c.first_name, c.last_name) AS 'full_name'
+FROM 
+	tickets AS ti
+INNER JOIN flights AS f
+ON 
+	f.flight_id = ti.flight_id
+INNER JOIN 
+	airports AS a
+ON 
+	a.airport_id=f.destination_airport_id
+INNER JOIN 
+	customers AS c
+ON 
+	c.customer_id=ti.customer_id
+WHERE
+	ti.price < 5000
+	AND
+	ti.class LIKE 'First'
+ORDER BY
+	ti.ticket_id;
+
+#10 Home town customers
+SELECT DISTINCT 
+	c.customer_id, 
+	CONCAT_WS(' ', c.first_name, c.last_name) AS 'full_name',
+	t.town_name
+FROM 
+	customers AS c
+INNER JOIN 
+	towns AS t
+ON 
+	t.town_id=c.home_town_id
+INNER JOIN 
+	airports AS a
+ON
+	c.home_town_id=t.town_id
+INNER JOIN 
+	flights AS f
+ON 
+	f.origin_airport_id=c.home_town_id
+ORDER BY
+	c.customer_id;
+
+
+
+
+	
