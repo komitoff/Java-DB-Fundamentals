9c4d3e0b29fd4bec769029fbdcbb07ab4675226d
komitoff <tobo_91@abv.bg>
2017-06-21 18:31:35 +0300

    Task 9 from the exam preparation part 2 - finished at 100/100

--- Exam Preparation/Exam Prep Part II/queries.sql
+++ Exam Preparation/Exam Prep Part II/queries.sql
@@ -40,12 +40,16 @@ ORDER BY
 	c.id,
 	m.id;
 	
-#9 not finished
+#9
 SELECT c.id, COUNT(m.chat_id) AS 'total_messages' 
 FROM chats AS c
 INNER JOIN messages AS m
 ON c.id = m.chat_id
-WHERE c.id = m.chat_id;
+AND m.id < 90
+WHERE c.id = m.chat_id
+GROUP BY c.id
+ORDER BY total_messages DESC, c.id 
+LIMIT 5;
 
 #10
 SELECT u.nickname, c.email, c.password
