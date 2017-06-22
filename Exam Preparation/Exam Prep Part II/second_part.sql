e6ef5c160f404c8f9a1004c18eb259c3876b9d03
komitoff <tobo_91@abv.bg>
2017-06-22 14:59:12 +0300

    Finished tasks 2 and 3 up to 100/100 from exam preparation part 2

--- Exam Preparation/Exam Prep Part II/second_part.sql
+++ Exam Preparation/Exam Prep Part II/second_part.sql
@@ -1,59 +1,41 @@
-#INSERT
+#INSERT 100 / 100
 INSERT INTO 
 	messages(content, sent_on, chat_id, user_id)
-		SELECT DISTINCT
+		SELECT
 		CONCAT_WS('-', u.age, u.gender, l.latitude, l.longitude) AS content,
 		'2016-12-15' AS sent_on,
 		CASE 
 			WHEN u.gender LIKE 'F' THEN
-				FLOOR(SQRT(u.age * 2))
+				CEIL(SQRT(u.age * 2))
 			WHEN u.gender LIKE 'M' THEN
-				FLOOR(POW(u.age / 18, 3))
+				CEIL(POW(u.age / 18, 3))
 		END AS 'chat_id',
 		u.id AS user_id
 	FROM users AS u
 	INNER JOIN locations AS l
 	ON u.location_id = l.id
-	INNER JOIN users_chats AS uc
-	ON u.id = uc.user_id
-	INNER JOIN chats AS c
-	ON c.id = uc.chat_id
-	WHERE u.id BETWEEN 10 AND 20;
+	AND u.id BETWEEN 10 AND 20;
 
-	
-SELECT DISTINCT
-	u.id,
-	CONCAT_WS('-', u.age, u.gender, l.latitude, l.longitude) AS content,
-	'2016-12-15' AS sent_on,
-	CASE 
-		WHEN u.gender LIKE 'f' THEN
-			CEIL(SQRT(u.age * 2))
-		WHEN u.gender LIKE 'm' THEN
-			CEIL(POW(u.age / 18, 3))
-	END AS 'chat_id',
-	u.id AS user_id
-FROM users AS u
-INNER JOIN locations AS l
-ON u.location_id = l.id
-INNER JOIN users_chats AS uc
-ON u.id = uc.user_id
-INNER JOIN chats AS c
-ON c.id = uc.chat_id
-WHERE u.id BETWEEN 10 AND 20;
 
-SELECT 
-	CASE 
-		WHEN u.gender LIKE 'f' THEN
-			FLOOR(SQRT(u.age * 2))
-		WHEN u.gender LIKE 'm' THEN
-			FLOOR(POW(u.age / 18, 3))
-	END AS 'age',
-FROM chats AS c
-INNER JOIN users_chats AS uc
-ON uc.chat_id = c.id
-INNER JOIN users AS u
-ON u.id = uc.user_id;
+#update
+UPDATE chats AS c
+INNER JOIN 
+	messages AS m
+ON 
+	c.id=m.chat_id
+SET c.start_date=m.sent_on
+WHERE
+	date(c.start_date) > date(m.sent_on);
 
+SELECT c.id, c.start_date, m.id, m.sent_on 
+FROM 
+	messages AS m
+INNER JOIN 
+	chats AS c
+ON 
+	c.id=m.chat_id
+AND 
+	date(c.start_date) > date(m.sent_on);
 
 #4 Delete
 DELETE
