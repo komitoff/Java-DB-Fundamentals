503f2562b4de76105033409dc3b30f234be091a9
komitoff <tobo_91@abv.bg>
2017-07-15 14:31:09 +0300

    Designed the database for the homework

---
+++ DBApp/minions_table_design.sql
@@ -0,0 +1,42 @@
+DROP DATABASE minions;
+
+CREATE DATABASE minions;
+
+CREATE TABLE countries (
+	country_id INT PRIMARY KEY,
+	country_name VARCHAR(150)
+);
+
+CREATE TABLE towns (
+	town_id INT PRIMARY KEY,
+	town_name VARCHAR(100) NOT NULL,
+	country_id INT,
+	CONSTRAINT fk_towns_countries
+	FOREIGN KEY (country_id) REFERENCES countries(country_id)
+);
+
+CREATE TABLE minions(
+	minion_id INT PRIMARY KEY,
+	minion_name VARCHAR(50) NOT NULL,
+	town_id INT NOT NULL,
+	CONSTRAINT fk_minions_towns
+	FOREIGN KEY (town_id) REFERENCES towns(town_id)
+);
+
+CREATE TABLE villains (
+	villain_id INT PRIMARY KEY,
+	villain_name VARCHAR(50) NOT NULL,
+	evilness_factor VARCHAR(10) 
+	CHECK( evilness_factor = 'good' OR evilness_factor = 'bad' OR evilness_factor = 'evil' OR evilness_factor = 'super evil')
+);
+
+CREATE TABLE villains_minions (
+	villain_id INT NOT NULL,
+	minion_id INT NOT NULL,
+	CONSTRAINT fk_villains_minions_minions
+	FOREIGN KEY (minion_id) REFERENCES minions(minion_id),
+	CONSTRAINT fk_villains_minions_villains
+	FOREIGN KEY (villain_id) REFERENCES villains(villain_id),
+	CONSTRAINT pk_villains_minions
+	PRIMARY KEY(villain_id, minion_id)
+);
--- DBApp/src/homework/AppConnection.java
+++ DBApp/src/homework/AppConnection.java
@@ -3,25 +3,24 @@ package homework;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class AppConnection {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/the_nerd_herd";
+    private static final String URL = "jdbc:mysql://localhost:3306/minions";
 
     private static final String USER = "root";
 
     private static final String PASSWORD = "";
 
     public static void main(String[] args) throws SQLException {
-        Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
-        System.out.println("Connected");
-        try {
-            
-        } finally {
-            try {
-                connection.close();
-            } catch (SQLException e) {
-                System.out.println(e.getMessage());
+        try(Connection connection = DriverManager.getConnection(URL, USER, PASSWORD)) {
+            try(Statement statement = connection.createStatement()) {
+                String sql = "CREATE TABLE countries(\n" +
+                        "\tcountry_id INT PRIMARY KEY,\n" +
+                        "\tcountry_name VARCHAR(150)\n" +
+                        ");";
+                statement.executeUpdate(sql);
             }
         }
     }
