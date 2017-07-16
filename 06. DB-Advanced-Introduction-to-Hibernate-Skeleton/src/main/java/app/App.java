589b5f80978e696b0b7ea365710dacd253e25f3a
komitoff <tobo_91@abv.bg>
2017-07-16 14:19:06 +0300

    Prepared the project structure and filled the database with some data.

---
+++ 06. DB-Advanced-Introduction-to-Hibernate-Skeleton/src/main/java/app/App.java
@@ -0,0 +1,11 @@
+package app;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class App {
+    public static void main(String[] args) {
+        EntityManagerFactory emf =
+                Persistence.createEntityManagerFactory("PersistenceUnit");
+    }
+}
