412c4841e75b5cdfaf5d94611199eb37286cf560
komitoff <tobo_91@abv.bg>
2017-07-25 19:22:30 +0300

    Finished with the design.

---
+++ spring_data_intro/student_system/src/main/java/student_system/entity/Homework.java
@@ -0,0 +1,78 @@
+package student_system.entity;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "homework")
+public class Homework {
+    private Long id;
+    private String content;
+    private Enum contentType;
+    private Date submissionDate;
+    private Student student;
+    private Course courses;
+
+    public Homework() {}
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Column(name = "content")
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Column(name = "content_type")
+    public Enum getContentType() {
+        return contentType;
+    }
+
+    public void setContentType(Enum contentType) {
+        this.contentType = contentType;
+    }
+
+    @Column(name = "submission_date")
+    public Date getSubmissionDate() {
+        return submissionDate;
+    }
+
+    public void setSubmissionDate(Date submissionDate) {
+        this.submissionDate = submissionDate;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "student_id",
+    referencedColumnName = "id",
+    foreignKey = @ForeignKey(name = "FK_homework_student"))
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "course_id",
+    referencedColumnName = "id",
+    foreignKey = @ForeignKey(name = "FK_homework_course"))
+    public Course getCourses() {
+        return courses;
+    }
+
+    public void setCourses(Course courses) {
+        this.courses = courses;
+    }
+}
--- spring_data_intro/student_system/src/main/java/student_system/entity/Student.java
+++ spring_data_intro/student_system/src/main/java/student_system/entity/Student.java
@@ -1,7 +1,81 @@
 package student_system.entity;
 
-/**
- * Created by tobieski on 7/25/2017.
- */
+import javax.persistence.*;
+import java.util.Date;
+import java.util.Set;
+
+@Entity
+@Table(name = "student")
 public class Student {
+    private Long id;
+    private String name;
+    private String phoneNumber;
+    private Date registrationDate;
+    private Date birthday;
+    private Set<Course> courses;
+
+    public Student() {}
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Column(name = "name")
+    @Basic(optional = false)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Column(name = "phone_number")
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Column(name = "registration_date")
+    @Basic(optional = false)
+    public Date getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(Date registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    @Column(name = "birthday")
+    @Basic(optional = false)
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    @ManyToMany
+    @JoinTable(name = "students_courses",
+    joinColumns = @JoinColumn(name = "student_id",
+    referencedColumnName = "id"),
+    inverseJoinColumns = @JoinColumn(name = "course_id",
+    referencedColumnName = "id"))
+    public Set<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(Set<Course> courses) {
+        this.courses = courses;
+    }
 }
