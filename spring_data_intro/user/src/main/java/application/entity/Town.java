e4ee1c5816530e872b3a32ab861cf611be8b4408
komitoff <tobo_91@abv.bg>
2017-07-26 14:48:05 +0300

    add table towns.

---
+++ spring_data_intro/user/src/main/java/application/entity/Town.java
@@ -0,0 +1,46 @@
+package application.entity;
+
+import org.springframework.stereotype.Component;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "town")
+public class Town {
+    private Long id;
+    private String name;
+    private List<User> users;
+
+    public Town() {}
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @OneToMany
+    @JoinColumn(name = "user_id",
+    referencedColumnName = "id")
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+}
